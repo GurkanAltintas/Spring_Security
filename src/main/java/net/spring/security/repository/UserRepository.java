04d4a8036c7466833bdef69d60151ab2af84ea43
gurkan <qurkan.a00@gmail.com>
2021-04-28 10:57:37 +0300

    custom authentication , user repository created

---
+++ src/main/java/net/spring/security/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package net.spring.security.repository;
+
+import net.spring.security.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users,Long> {
+
+    Users findByUsername(String username);
+
+
+
+}
---
+++ src/main/java/net/spring/security/service/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package net.spring.security.service;
+
+import lombok.RequiredArgsConstructor;
+import net.spring.security.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return userRepository.findByUsername(s);
+    }
+}
